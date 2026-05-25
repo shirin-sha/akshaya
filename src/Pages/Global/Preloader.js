693694ba2c829d8771d8bec95ed807e2058c2dad
shirin-sha <project.manager4565@gmail.com>
2026-05-25 17:48:55 +0530

    loading

--- src/Pages/Global/Preloader.js
+++ src/Pages/Global/Preloader.js
@@ -1,29 +1,37 @@
 "use client"
 import React, { useState, useEffect } from 'react';
 
+const LOADING_TEXT = 'ALSHAYA ENTERPRISES';
+
 export default function Preloader() {
     const [isLoading, setIsLoading] = useState(true);
+
     useEffect(() => {
         const timer = setTimeout(() => {
             setIsLoading(false);
         }, 2500);
         return () => clearTimeout(timer);
-    });
+    }, []);
+
+    if (!isLoading) {
+        return null;
+    }
+
     return (
-        isLoading && (
-            <div className="preloader">
-                <div className="loading-container">
-                    <div className="loading-text">
-                        <span>A</span>
-                        <span>L</span>
-                        <span>S</span>
-                        <span>H</span>
-                        <span>A</span>
-                        <span>Y</span>
-                        <span>A</span>
-                    </div>
+        <div className="preloader">
+            <div className="loading-container">
+                <div className="loading-text">
+                    {LOADING_TEXT.split('').map((letter, index) => (
+                        <span
+                            key={index}
+                            className={letter === ' ' ? 'loading-text-space' : undefined}
+                            style={{ animationDelay: `${(index + 1) * 0.05}s` }}
+                        >
+                            {letter === ' ' ? '\u00A0' : letter}
+                        </span>
+                    ))}
                 </div>
             </div>
-        )
-    )
+        </div>
+    );
 }
